2bce7ef2a33063a0c78d542aad99c4a8de6f6e1b
110598010 <t110598010@ntut.org.tw>
2021-09-29 20:50:29 +0800

    test fixture

--- test/test_shape.h
+++ test/test_shape.h
@@ -6,37 +6,54 @@
 #include "../src/circle.h"
 #include "../src/shape.h"
 
-TEST(CaseShape, CreationShape) {
-    Shape* s = new Square(10.0);
-    Circle* c = new Circle(10.0);
-    ASSERT_NEAR(100, s->area(), 0.0001);
-    ASSERT_NEAR(40, s->perimeter(), 0.0001);
+class CaseShape : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        s10 = new Square(10.0);
+        c10 = new Circle(10.0);
+    }
 
-    ASSERT_NEAR(314.15, c->area(), 0.1);
-    ASSERT_NEAR(62.83, c->perimeter(), 0.1);
+    void TearDown() override
+    {
+        delete s10;
+        delete c10;
+    }
+
+    Shape *s10;
+    Shape *c10;
+};
+
+TEST_F(CaseShape, CreationShape)
+{
+    ASSERT_NEAR(100, s10->area(), 0.0001);
+    ASSERT_NEAR(40, s10->perimeter(), 0.0001);
+
+    ASSERT_NEAR(314.15, c10->area(), 0.1);
+    ASSERT_NEAR(62.83, c10->perimeter(), 0.1);
 }
 
-TEST(CaseShape, SortIncreasing) {
-    std::vector<Shape*> shapes;
-    shapes.push_back(new Circle(10.0));
-    shapes.push_back(new Square(10.0));
-    std::sort(shapes.begin(), shapes.end(), [](Shape* s1, Shape* s2) {
-        return s1->area() < s2->area();
-    });
-    ASSERT_NEAR(100, shapes[0]->area(),0.0001);
-    ASSERT_NEAR(314.159, shapes[1]->area(),0.01);
+TEST_F(CaseShape, SortIncreasing)
+{
+    std::vector<Shape *> shapes;
+    shapes.push_back(c10);
+    shapes.push_back(s10);
+    std::sort(shapes.begin(), shapes.end(), [](Shape *s1, Shape *s2)
+              { return s1->area() < s2->area(); });
+    ASSERT_NEAR(100, shapes[0]->area(), 0.0001);
+    ASSERT_NEAR(314.159, shapes[1]->area(), 0.01);
 }
 
-// lambda
-TEST(CaseShape, SortDescending) {
-    std::vector<Shape*> shapes;
-    shapes.push_back(new Square(10.0));
-    shapes.push_back(new Circle(10.0));
+TEST_F(CaseShape, SortDescending)
+{
+    std::vector<Shape *> shapes;
+    shapes.push_back(s10);
+    shapes.push_back(c10);
 
-    std::sort(shapes.begin(), shapes.end(), [](Shape* s1, Shape* s2) {
-        return s1->area() >= s2->area();
-    });
+    std::sort(shapes.begin(), shapes.end(), [](Shape *s1, Shape *s2)
+              { return s1->area() >= s2->area(); });
 
-    ASSERT_NEAR(314.159, shapes[0]->area(),0.1);
-    ASSERT_NEAR(100, shapes[1]->area(),0.0001);
+    ASSERT_NEAR(314.159, shapes[0]->area(), 0.1);
+    ASSERT_NEAR(100, shapes[1]->area(), 0.0001);
 }
