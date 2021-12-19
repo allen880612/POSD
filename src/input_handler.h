12428f66de97e0aef52e5303cc7f21f48641eaa1
110598010 <110598010@ntut.org.tw>
2021-12-19 22:05:59 +0800

    complete main

--- src/input_handler.h
+++ src/input_handler.h
@@ -1,23 +1,31 @@
 #pragma once
 
 #include <iostream>
-// #include "./builder/shape_builder.h"
+using namespace std;
 
 class ShapeBuilder;
 class InputHandler {
 public:
+    InputHandler();
     void handle();
 
 private:
     void printEditorInstructions();
     void handleEditorInstructions(int instruction);
-    void save();
+    void InputEditorInstruction();
+    
     void addCircle();
     void addRectangle();
     void addTriangle();
+    
     void printCompoundInstructions();
     void handleCompoundInstructions(int instruction);
+    void InputCompoundInstruction();
     void addCompound();
+    void endCompound();
+    
+    void save();
+    void handleInvalidInput(string msg);
 
     ShapeBuilder* builder;
     bool isContinued = true;
